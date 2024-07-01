f800a68961d03f8af395fbb1574c77a775fc4a1f
Roberto <robservil@alumn.us.es>
2024-07-02 00:52:50 +0200

    prueba

--- DeliverUS-Backend/src/controllers/RestaurantController.js
+++ DeliverUS-Backend/src/controllers/RestaurantController.js
@@ -22,19 +22,81 @@ const index = async function (req, res) {
 // TODO: Complete the following functions
 
 const create = async function (req, res) {
-
+  const newRestaurant = Restaurant.build(req.body)
+  newRestaurant.userId = 1
+  try {
+    const restaurant = await newRestaurant.save()
+    res.json(restaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const show = async function (req, res) {
-
+  try {
+    const restaurantId = req.params.restaurantId
+    const restaurant = await Restaurant.findByPk(
+      restaurantId,
+      {
+        attributes: { exclude: ['userId'] },
+        include: [
+          {
+            model: RestaurantCategory,
+            as: 'restaurantCategory'
+          },
+          {
+            model: Product,
+            as: 'products',
+            include: [
+              {
+                model: ProductCategory,
+                as: 'productCategory'
+              }
+            ]
+          }
+        ],
+        order: [[{ model: Product, as: 'products' }, 'order', 'ASC']]
+      }
+    )
+    if (restaurant) {
+      res.json(restaurant)
+    } else {
+      res.status(404).send('Restaurant not found')
+    }
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const update = async function (req, res) {
-
+  try {
+    const restaurantId = req.params.restaurantId
+    await Restaurant.update(req.body, { where: { id: restaurantId } })
+    const updatedRestaurant = await Restaurant.findByPk(restaurantId)
+    res.json(updatedRestaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const destroy = async function (req, res) {
-
+  try {
+    let message = ''
+    const restaurantId = req.params.restaurantId
+    const restaurant = await Restaurant.destroy(
+      {
+        where: { id: restaurantId }
+      }
+    )
+    if (restaurant === 1) {
+      message = 'Sucessfuly deleted restaurant id.' + restaurantId
+    } else {
+      message = 'Could not delete restaurant with id.' + restaurantId
+    }
+    res.json(message)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const RestaurantController = {
--- DeliverUS-Backend/src/routes/RestaurantRoutes.js
+++ DeliverUS-Backend/src/routes/RestaurantRoutes.js
@@ -1,7 +1,14 @@
 import RestaurantController from '../controllers/RestaurantController.js'
 
 const loadFileRoutes = function (app) {
-  // TODO: Include routes for restaurant described in the lab session README
+  app.route('/restaurants')
+    .get(RestaurantController.index)
+    .post(RestaurantController.create)
 
+  app.route('/restaurants/:restaurantId')
+    .get(RestaurantController.show)
+    .put(RestaurantController.update)
+    .delete(RestaurantController.destroy)
 }
+
 export default loadFileRoutes
